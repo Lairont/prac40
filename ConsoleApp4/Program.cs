dc7b3129c07c68b2db2b31b097503fb8d7ad2b90
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-07 23:09:00 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+            delegate double AverageDelegate(int m, int n);
+
+            static void Main(string[] args)
+            {
+                IOAverage();
+                IOAverage();
+                IOAverage();
+            }
+            static void IOAverage()
+            {
+            AverageDelegate averageDelegate = Average;
+
+                Console.Write("Первое число = ");
+                int m = int.Parse(Console.ReadLine());
+
+                Console.Write("Второе число = ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Среднее арифметическое чисел от {m} до {n}: {averageDelegate.Invoke(m, n)}\n");
+                Console.Read();
+            }
+    public static double Average(int m, int n)
+        {
+            return (m + n) / 2.0;
+        }
+    }
+}
